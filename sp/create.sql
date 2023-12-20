0ce9840c3e2b1e213be761421c00517bd4954f89
Javier Ponce <aquosoft@gmail.com>
2023-12-20 13:21:50 -0300

    edit create

--- sp/create.sql
+++ sp/create.sql
@@ -1,3 +1,12 @@
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY '123456789';
+
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '123456789';
+FLUSH PRIVILEGES;
+
+
+
+
 CREATE SCHEMA `db_persist` ;
 
 CREATE TABLE `db_persist`.`contadorpdf` (
